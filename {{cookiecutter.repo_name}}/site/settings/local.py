c53363e67eb295d6ae13b9389d12a4624b97b66d
Tzu-ping Chung <uranusjr@gmail.com>
2016-09-06 12:51:39 +0800

    Setup Debug Toolbar explicitly as recommended

--- {{cookiecutter.repo_name}}/site/settings/local.py
+++ {{cookiecutter.repo_name}}/site/settings/local.py
@@ -3,9 +3,11 @@ from .base import *     # noqa
 
 DEBUG = True
 
-INSTALLED_APPS += [
+INSTALLED_APPS += [     # noqa
     'debug_toolbar',
     'djgunicorn',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
