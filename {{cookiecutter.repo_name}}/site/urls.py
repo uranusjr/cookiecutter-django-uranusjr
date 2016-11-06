9242afcc9a0e4bb1766c0ddca94f1c8ffb4ef0b3
Tzu-ping Chung <uranusjr@gmail.com>
2016-11-06 16:40:13 +0800

    Add media-serving logic

--- {{cookiecutter.repo_name}}/site/urls.py
+++ {{cookiecutter.repo_name}}/site/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.static import serve as serve_static
 
 
 urlpatterns = [
@@ -11,5 +12,7 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve_static,
+            {'document_root': settings.MEDIA_ROOT}),
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
